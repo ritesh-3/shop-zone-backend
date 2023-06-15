a904c8ffae98546ec468058efec587b4896f8fec
ritesh81269@gmail.com <ritesh81269@gmail.com>
2023-06-15 23:40:33 +0530

    disabled cache for token call

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,7 +1,8 @@
 // create token and saving that in cookies
 const sendToken = (user, statusCode, res) => {
   const token = user.getJwtToken();
-
+ // Set cache-control header to no-store
+ res.setHeader('Cache-Control', 'no-store');
   // Options for cookies
   const options = {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
